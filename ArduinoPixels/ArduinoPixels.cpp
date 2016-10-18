d640a5500ccdcb48db34ddcaae40f60634d256b6
Doug Schaefer <dschaefer@blackberry.com>
2016-10-18 15:00:37 -0400

    Add the Arduino project.

---
+++ ArduinoPixels/ArduinoPixels.cpp
@@ -0,0 +1,17 @@
+#include <Arduino.h>
+#include <Adafruit_NeoPixel.h>
+
+Adafruit_NeoPixel pixels(4, 13);
+
+void setup() {
+	pixels.begin();
+}
+
+void loop() {
+	for (int i = 0; i < 4; i++) {
+		pixels.setPixelColor(i, rand(), rand(), rand());
+	}
+	pixels.show();
+
+	delay(200);
+}
